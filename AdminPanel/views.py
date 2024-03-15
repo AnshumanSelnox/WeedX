1edb6d0d0f131b43fd5d601bda5e56dd02f07361
RiseeSelnox <selnoxs001@gmail.com>
2024-03-15 18:05:08 +0530

    sqdx

--- AdminPanel/views.py
+++ AdminPanel/views.py
@@ -3817,4 +3817,3 @@ class UpdateAdminProfile(APIView):
                     return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({'error' : str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
